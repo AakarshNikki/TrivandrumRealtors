a2b764e11ffd5345bd697ed706593ac97c17682a
AakarshNikki <66575261+AakarshNikki@users.noreply.github.com>
2024-10-22 00:25:24 +0530

    Update script.js
    fullpage edit-js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,56 @@
-const hamburger = document.getElementById('hamburger');
+
 const navLinks = document.getElementById('nav-links');
+$(document).ready(function() {	
+	$('.current-projects').slick({
+		dots: true,
+		infinite: false,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 3,
+			  slidesToScroll: 3,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 600,
+			settings: {
+			  slidesToShow: 2,
+			  slidesToScroll: 2,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  slidesToShow: 1,
+			  slidesToScroll: 1,
+			  infinite: true,
+			  dots: true
+			}
+		  }
+		]
+	  });
+	/* Navigation burger onclick side navigation show */
+	$('.hamburger').on('click', function() {
+		if($('#myBtn').hasClass('change')) {
+			$('.nav-links').addClass('stop-scroll');
+            $('.nav-links').toggle('slow');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navLinks.classList.toggle('active');
+		} else {
+            $('.nav-links').toggle('slow');
+			$('.nav-links').removeClass('stop-scroll');
+		}
+	});
 });
+new WOW().init();
 
-$(document).ready(function(){
-    $('.current-projects').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
-  });
-
-  new WOW().init();
+ 
